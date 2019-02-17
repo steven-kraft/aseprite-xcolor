323357b0eae5165a0e1a923090243db4a2512bf4
Steven Kraft <steven.kraftjp@gmail.com>
2019-02-16 23:29:48 -0500

    Modified to get absolute script path

--- aseprite-xcolor.lua
+++ aseprite-xcolor.lua
@@ -1,5 +1,8 @@
-os.execute("./xcolor -f plain > ./color.txt")
-local file = io.open("color.txt", "r")
+local str = debug.getinfo(2, "S").source:sub(2)
+local script_path = str:match("(.*/)")
+
+os.execute(script_path .. "xcolor" ..  " -f plain > " .. script_path .. "color.txt")
+local file = io.open(path .. "color.txt", "r")
 io.input(file)
 local color = io.read()
 io.close()
